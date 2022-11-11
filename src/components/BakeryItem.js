bee061fd20c13a23915fca1a41cbc4ef73276a0b
Anna Zhao <68annazhao@gmail.com>
2022-11-10 19:05:08 -0500

    done

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,14 @@
 // TODO: create a component that displays a single bakery item
+function BakeryItem({item, addToCart}) {
+    return (
+      <div>
+        <img src={item.image} alt="hello"/>
+        <h3>{item.name}</h3>
+        <h4>{item.description}</h4>
+        <p>Price: {item.price}</p>
+        <button onClick={() => addToCart(item)} >Add To Cart</button>
+      </div>
+    )
+  }
+
+export default BakeryItem;
